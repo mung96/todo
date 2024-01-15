abdc835974dab74c756095a3077e5ab4938c18cc
mung96 <mung5079@gmail.com>
2024-01-15 19:43:30 +0900

    feat:새로고침해도 데이터 유지

--- makeToDo.js
+++ makeToDo.js
@@ -1,9 +1,17 @@
 (()=>{
-
     const submitBtn = document.querySelector('.submit-todolist');
     const impoElem= document.querySelector('.choice-importance');
-  
+    let id=0;
 
+    const jsonLocalStorage = {
+        setItem: (key, value) => {
+          localStorage.setItem(key, JSON.stringify(value));
+        },
+        getItem: (key) => {
+          return JSON.parse(localStorage.getItem(key));
+        },
+      };
+  
     function makeToggle(){
         let newToggle = document.createElement("input");
         newToggle.role = "switch";
@@ -23,10 +31,19 @@
         return newImpo;
     }
 
-    function makeDeleteBtn(newLabel){
+    function makeDeleteBtn(newLabel,todo){
         let newBtn = document.createElement("button");
         newBtn.textContent="삭제";
         newBtn.addEventListener('click',()=>{
+            let currentTodos = jsonLocalStorage.getItem('todos')||[];
+            console.log(currentTodos);
+            for(let i=0;i<currentTodos.length;i++){
+                if(todo.id === currentTodos[i].id){
+                    currentTodos.splice(i,1);
+                    break;
+                }
+            }
+            jsonLocalStorage.setItem('todos',currentTodos);
             newLabel.remove();
         })
         return newBtn;
@@ -38,7 +55,8 @@
         newLabel.appendChild(makeToggle());
         newLabel.appendChild(makeText(todo));
         newLabel.appendChild(makeImpo(todo));
-        newLabel.appendChild(makeDeleteBtn(newLabel));
+        newLabel.appendChild(makeDeleteBtn(newLabel,todo));
+        console.log(newLabel);
         return newLabel;
     }
 
@@ -65,12 +83,26 @@
     })
 
     submitBtn.addEventListener('click',(e)=>{
+        let todos = jsonLocalStorage.getItem('todos')||[];
         const textElem = document.querySelector('.text-todolist');
-        let todo = {text:textElem.value,impo:impoElem.value};
+
+        let todo = {id:id, text:textElem.value,impo:impoElem.value};
+        todos.push(todo);
+        jsonLocalStorage.setItem('todos',todos);
+
+        id +=1;
+
         if(!impoElem.value){todo.impo="1";}
         e.preventDefault();
         addView(todo);
         textElem.value='';
         impoElem.value='';
     })
+
+    window.onload= ()=>{
+        todos = jsonLocalStorage.getItem('todos')||[];
+        for(todo of todos){
+            addView(todo);
+        }
+    }
 })();
