a470b6334b35d16b024b65b7570771e2232e7778
송현명 <mung5079@naver.com>
2024-01-16 08:36:40 +0900

    feat: 전체 모아보기 기능 구현(in '중요도 별로 모아보기')

--- seeByImpo.js
+++ seeByImpo.js
@@ -5,15 +5,30 @@
 
     impo.addEventListener('change',()=>{
         const currentSelect = impo.options[impo.selectedIndex].value;
-        const components = document.querySelector(`.component-importance[data-impo="${currentSelect}"]`);
-        const labels = components.querySelectorAll('label');
-
         contents.innerHTML='';
-        for (let label of labels){
-            const text = label.querySelector('.view-todolist-text').textContent
-            const newLi = document.createElement('li');
-            newLi.textContent = text;
-            contents.appendChild(newLi);
+
+        if(currentSelect in [1,2,3]){
+            const components = document.querySelector(`.component-importance[data-impo="${currentSelect}"]`);
+            const labels = components.querySelectorAll('label');
+            for (let label of labels){
+                const text = label.querySelector('.view-todolist-text').textContent
+                const newLi = document.createElement('li');
+                newLi.textContent = text;
+                contents.appendChild(newLi);
+            }
+        }
+        
+        if (currentSelect==="전체"){
+            const allComponents = document.querySelectorAll('.component-importance');
+            for (let component of allComponents){
+                component.querySelectorAll('label').forEach(function(label){
+                    const text = label.querySelector('.view-todolist-text').textContent
+                    const newLi = document.createElement('li');
+                    newLi.textContent = text;
+                    contents.appendChild(newLi);
+                });
+
+            }
         }
     })
 })();
